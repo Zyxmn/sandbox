f273db3ccf6c8e81b8f07e53c1a279b5fd72ca28
Zyxmn Daley Jes <zyx@juxt.pro>
2020-03-07 14:36:09 +0000

    zip content read save

--- recipe-search/src/recipe_search/core.clj
+++ recipe-search/src/recipe_search/core.clj
@@ -1,4 +1,6 @@
 (ns recipe-search.core
+  (:require [clojure.java.io :as io])
+  (:import java.util.zip.ZipInputStream)
   (:gen-class))
 
 (defn -main
@@ -6,9 +8,26 @@
   [& args]
   (println "Hello, World!"))
 
+(defn filenames-in-zip [filename]
+  (let [z (java.util.zip.ZipFile. filename)]
+    (map #(.getName %) (enumeration-seq (.entries z)))))
 
 (comment
 
-(-main)
+  (-main)
 
-         )
+  (filenames-in-zip "resources/recipes.zip")
+  (def zs (ZipInputStream. (io/input-stream "resources/recipes.zip")))
+  (def ze (.getNextEntry zs))
+  (def zip-entry (-> (java.util.zip.ZipFile.  "resources/recipes.zip")
+                     (.getEntry "recipes/afelia.txt")))
+  (-> (java.util.zip.ZipFile.  "resources/recipes.zip")
+      (.getInputStream zip-entry)
+      (slurp))
+
+
+  (with-open [in (type (ZipInputStream.
+                   (clojure.java.io/input-stream
+                     "resources/recipes.zip")))]
+    (slurp in))
+  )
