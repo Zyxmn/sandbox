a69dc70cb8d1b68826b3ea1e07ba09b11b9736d0
Zyxmn Daley Jes <zyx@juxt.pro>
2020-03-07 16:12:27 +0000

    simple search save

--- recipe-search/src/recipe_search/core.clj
+++ recipe-search/src/recipe_search/core.clj
@@ -1,6 +1,4 @@
 (ns recipe-search.core
-  (:require [clojure.java.io :as io])
-  (:import java.util.zip.ZipInputStream)
   (:gen-class))
 
 (defn -main
@@ -19,6 +17,18 @@
         filenames-in-zip (map #(.getName %) (enumeration-seq (.entries z)))]
     (map #(-> z (.getEntry %)) filenames-in-zip)))
 
+(defn zip-content->map-seq
+  "Read a zipfile and return a lazyseq of maps with filename as key and
+  file content input stream as value"
+  [zipfile]
+  (let [z (java.util.zip.ZipFile. zipfile)
+        filenames-in-zip (map #(.getName %) (enumeration-seq (.entries z)))
+        entries-in-zip (map #(-> z (.getEntry %)) filenames-in-zip)]
+    (map #(let [recipe-name (str (.getName %))
+                recipe-description (-> z (.getInputStream %))]
+            {(keyword recipe-name) recipe-description})
+         entries-in-zip)))
+
 (defn zip-content->map
   "Read a zipfile and return a lazyseq of maps with filename as key and its
   contents as value"
@@ -31,10 +41,19 @@
             {(keyword recipe-name) recipe-description})
          entries-in-zip)))
 
+(def recipe-list (zip-content->map "resources/recipes.zip"))
+
+
 (comment
 
   (-main)
 
+  ; search
+  (map #(let [recipe-map (-> % first )]
+          {:match-count (count (re-seq #"olive oil" (val recipe-map)))
+           :recipe (key recipe-map)}) recipe-list)
+
+  (count (re-seq #"yolo" "yolo yolo1 and yolo2"))
   (filenames-in-zip "resources/recipes.zip")
   (entries-in-zip "resources/recipes.zip")
   (second (zip-content->map "resources/recipes.zip"))
