ad9f4ca2792a3a025a411a4db2236d9d2e34acf8
Zyxmn Daley Jes <zyx@juxt.pro>
2020-03-08 12:58:12 +0000

    add search function

--- recipe-search/src/recipe_search/core.clj
+++ recipe-search/src/recipe_search/core.clj
@@ -1,48 +1,39 @@
 (ns recipe-search.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
-(defn filenames-in-zip [filename]
-  "Return a list of filenames present inside the zip file"
-  (let [z (java.util.zip.ZipFile. filename)]
-    (map #(.getName %) (enumeration-seq (.entries z)))))
-
-(defn entries-in-zip [filename]
-  "Return a list of zip entries present inside the zip file"
-  (let [z (java.util.zip.ZipFile. filename)
-        filenames-in-zip (map #(.getName %) (enumeration-seq (.entries z)))]
-    (map #(-> z (.getEntry %)) filenames-in-zip)))
-
-(defn zip-content->map-seq
-  "Read a zipfile and return a lazyseq of maps with filename as key and
-  file content input stream as value"
-  [zipfile]
-  (let [z (java.util.zip.ZipFile. zipfile)
-        filenames-in-zip (map #(.getName %) (enumeration-seq (.entries z)))
-        entries-in-zip (map #(-> z (.getEntry %)) filenames-in-zip)]
-    (map #(let [recipe-name (str (.getName %))
-                recipe-description (-> z (.getInputStream %))]
-            {(keyword recipe-name) recipe-description})
-         entries-in-zip)))
-
 (defn zip-content->map
   "Read a zipfile and return a lazyseq of maps with filename as key and its
-  contents as value"
+  description as value"
   [zipfile]
   (let [z (java.util.zip.ZipFile. zipfile)
         filenames-in-zip (map #(.getName %) (enumeration-seq (.entries z)))
         entries-in-zip (map #(-> z (.getEntry %)) filenames-in-zip)]
     (map #(let [recipe-name (.getName %)
                 recipe-description (-> z (.getInputStream %) slurp)]
-            {(keyword recipe-name) recipe-description})
+            {recipe-name recipe-description})
          entries-in-zip)))
 
 (def recipe-list (zip-content->map "resources/recipes.zip"))
 
+(defn search-recipe
+  "A simple search that matches the whole phrase with the recipe list
+  and returns the top 10 hits (most occurence of given phrase in a recipe's
+  description), if there are no hits it returns an empty list"
+  [phrase]
+  (let [recipe-list (zip-content->map "resources/recipes.zip")]
+    (->> (map #(let [recipe-map (-> % first)]
+                 {:match-count (count (re-seq (re-pattern (str phrase)) (val recipe-map)))
+                  :recipe (key recipe-map)})
+              recipe-list)
+         (sort-by :match-count)
+         (filter #(> (:match-count %) 0))
+         reverse
+         (take 10))))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println (str args "Hello, World!")))
 
 (comment
 
@@ -50,25 +41,15 @@
 
   ; search
   (def search-test (map #(let [recipe-map (-> % first )]
-          {:match-count (count (re-seq #"olive oil" (val recipe-map)))
-           :recipe (key recipe-map)}) recipe-list))
+                           {:match-count (count (re-seq #"olive oil" (val recipe-map)))
+                            :recipe (key recipe-map)}) recipe-list))
   (reverse (sort-by :match-count search-test))
 
-  (count (re-seq #"yolo" "yolo yolo1 and yolo2"))
-  (filenames-in-zip "resources/recipes.zip")
-  (entries-in-zip "resources/recipes.zip")
-  (second (zip-content->map "resources/recipes.zip"))
-  (def zs (ZipInputStream. (io/input-stream "resources/recipes.zip")))
-  (def ze (.getNextEntry zs))
-  (def zip-entry (-> (java.util.zip.ZipFile.  "resources/recipes.zip")
-                     (.getEntry "recipes/afelia.txt")))
-  (-> (java.util.zip.ZipFile.  "resources/recipes.zip")
-      (.getInputStream zip-entry)
-      slurp)
-
-
-  (with-open [in (type (ZipInputStream.
-                   (clojure.java.io/input-stream
-                     "resources/recipes.zip")))]
-    (slurp in))
-  )
+  (time (search-recipe "tomato"))
+  (time (search-recipe "broccoli stilton soup"))
+  (search-recipe "broccoli")
+  (search-recipe "stilton")
+  (search-recipe "lettuce soup")
+  (time (+ 1 2))
+  (time (second (zip-content->map "resources/recipes.zip")))
+)
