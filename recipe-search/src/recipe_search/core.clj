33cded37925c752302baaebf586abf1a31bfa820
Zyxmn Daley Jes <zyx@juxt.pro>
2020-03-08 13:56:15 +0000

    add first draft of search

--- recipe-search/src/recipe_search/core.clj
+++ recipe-search/src/recipe_search/core.clj
@@ -16,40 +16,40 @@
 (def recipe-list (zip-content->map "resources/recipes.zip"))
 
 (defn search-recipe
-  "A simple search that matches the whole phrase with the recipe list
+  "A simple search that matches the whole phrase with the recipe description
   and returns the top 10 hits (most occurence of given phrase in a recipe's
   description), if there are no hits it returns an empty list"
   [phrase]
-  (let [recipe-list (zip-content->map "resources/recipes.zip")]
-    (->> (map #(let [recipe-map (-> % first)]
-                 {:match-count (count (re-seq (re-pattern (str phrase)) (val recipe-map)))
-                  :recipe (key recipe-map)})
-              recipe-list)
-         (sort-by :match-count)
-         (filter #(> (:match-count %) 0))
-         reverse
-         (take 10))))
+  (->> (pmap #(let [recipe-map (-> % first)
+                    recipe-name (key recipe-map)
+                    recipe-description (val recipe-map)]
+                {:match-count (count (re-seq (re-pattern (str phrase)) recipe-description))
+                 :recipe-name recipe-name})
+             recipe-list)
+       (sort-by :match-count)
+       (filter #(> (:match-count %) 0))
+       reverse
+       (take 10)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println (str args "Hello, World!")))
+  "Print out relevant recipes in order"
+  [phrase]
+  (let [relevant-recipes (search-recipe phrase)]
+    (doseq [single-recipe relevant-recipes]
+      (println (-> (:recipe-name single-recipe)
+                   (clojure.string/replace #"recipes/" ""))))))
+
 
 (comment
 
-  (-main)
+  ;Example Usage of search function
 
-  ; search
-  (def search-test (map #(let [recipe-map (-> % first )]
-                           {:match-count (count (re-seq #"olive oil" (val recipe-map)))
-                            :recipe (key recipe-map)}) recipe-list))
-  (reverse (sort-by :match-count search-test))
-
-  (time (search-recipe "tomato"))
-  (time (search-recipe "broccoli stilton soup"))
   (search-recipe "broccoli")
   (search-recipe "stilton")
   (search-recipe "lettuce soup")
-  (time (+ 1 2))
-  (time (second (zip-content->map "resources/recipes.zip")))
-)
+
+  (time (search-recipe "tomato"))
+  (time (search-recipe "broccoli stilton soup"))
+
+
+  )
