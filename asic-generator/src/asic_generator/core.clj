3d1109172f0a6d34cd1f400f845251123b633774
Zyxmn Daley Jes <zyx@juxt.pro>
2020-03-15 15:28:41 +0000

    sort of working asci converter

--- asic-generator/src/asic_generator/core.clj
+++ asic-generator/src/asic_generator/core.clj
@@ -21,27 +21,28 @@
 
 (defn hash-replacement
   [input-string]
- (let [no-of-hash (count (re-seq #"#" input-string))
-       hash-regex (apply str (repeat no-of-hash "#"))
-       no-of-tabs (apply str (repeat (- no-of-hash 1) "\t"))]
-   (case no-of-hash
-     1 (clojure.string/replace input-string #"#" "\u2514\u2500")
-     2 (clojure.string/replace input-string #"##" "\t\u251C\u2500")
-     (>= no-of-hash 3) (clojure.string/replace input-string (re-pattern hash-regex)
-                                               (str "\t\u2502" no-of-tabs "\u2514\u2500"))
-     input-string)))
+  (let [no-of-hash (count (re-seq #"#" input-string))
+        hash-regex (apply str (repeat no-of-hash "#"))
+        no-of-tabs (apply str (repeat (- no-of-hash 1) "\t"))]
+    (println "no-of-hash: " no-of-hash)
+    (println "hash-regex" hash-regex)
+    (cond
+      (= no-of-hash 1) (clojure.string/replace input-string #"#" "\u2514\u2500")
+      (= no-of-hash 2) (clojure.string/replace input-string #"##" "\t\u251C\u2500")
+      (>= no-of-hash 3) (clojure.string/replace input-string (re-pattern hash-regex)
+                                                (str "\t\u2502" no-of-tabs "\u2514\u2500")))))
 
 (defn table-of-contents
   [input-string]
   (let [vec-string (clojure.string/split input-string #"\n")
         hash-replaced-string (map hash-replacement vec-string)
         add-new-line (map #(str % "\n") hash-replaced-string)]
-  (println (apply str add-new-line))))
+    (println (apply str add-new-line))))
 
 (comment
 
 (first (table-of-contents input-string))
-
+(>= 4 3)
 (apply str (repeat 3 "#"))
 (apply str (vec input-string))
 (vector input-string)
