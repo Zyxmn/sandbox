d5d6f52b9f6c8447563e8e1b38c8d4ac66460a28
Zyxmn <zyxmndaleyjes@gmail.com>
2025-01-23 13:48:43 +0000

    noodling

--- pedestal-tutorial/src/hello.clj
+++ pedestal-tutorial/src/hello.clj
@@ -4,3 +4,35 @@
 
 
 (prn "yolo")
+
+(defn hello-world [arg]
+  (println "yolo"))
+
+(map inc [1 2 3])
+
+(reduce (fn [a b] (+ a b)) 1 [2 3 4])
+
+(comment
+
+ (defn drop-nth-items [coll n]
+   (->> coll
+       (partition-all n)
+       (map #(if (= (count %) n)
+               (drop-last %)
+               %))
+       flatten))
+
+ (drop-nth-items  [1 2 3 4 5 6 7 8] 3)
+
+ (<= 1 1 )
+
+(defn factorial! [n]
+  (loop [total 1
+         next-num n]
+    (if (>= 1 next-num)
+      total
+      (recur (* total next-num) (dec next-num)))))
+
+ (factorial! 4)
+
+  )
