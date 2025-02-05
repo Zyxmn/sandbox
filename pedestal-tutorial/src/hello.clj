a17b85fa68a902d04f241b36aeab8b24c1acc16a
Zyxmn <zyxmndaleyjes@gmail.com>
2025-02-05 17:03:59 +0000

    noodling

--- pedestal-tutorial/src/hello.clj
+++ pedestal-tutorial/src/hello.clj
@@ -1,6 +1,6 @@
 (ns hello
   (:import (java.util Date)
-           (java.time Date))
+           (java.time LocalDate))
   (:gen-class))
 
 (defn -main [& args]
@@ -20,17 +20,41 @@
       (println hours)
       (str hours ":" minutes ":" seconds"."last-two-digits)))
 
-   123456656456
+
 
 (->> 366144 str vec (take-last 2) (apply str))
- 3600 60 1 44
 
 (quartz-sec 89999999999999999999)
 (format "%.2f" (float (/ 366144 100)))
 (int (/ 366144 100))
-(Date. )
+(.getYear (Date. ))
+
+(Date.)
 
 (int (/ 7 5))
+
+(defn custom-calendar
+  "Given date return the current week and the week before and after given date"
+  [year month day]
+  (->> (str year "-" month "-" day)
+       (. LocalDate parse) ; .method doesnt work on static methods it looks like so use class/static-method or (. class static-method)
+       (.atStartOfDay)))
+
+;LocalDate.parse(str "2015-12-22").atStartOfDay()
+
+(import (java.time LocalDate))
+
+(.of LocalDate "2015-12-22")
+
+(LocalDate/now)
+(.now LocalDate) ; doesnt work cause its a static method i think
+(LocalDate/of 2015 12 22)
+(. LocalDate of 2015 12 22)
+(.atStartOfDay (. LocalDate parse "2015-12-22"))
+
+(custom-calendar 2015 12 21)
+
+
 
 
 )
